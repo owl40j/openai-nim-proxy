0d0f078a7efa63b398a9ff414fb40c94955fb122
owl40j <kalelalfortejose@gmail.com>
2026-04-22 03:05:41 +0800

    Update server.js

--- server.js
+++ server.js
@@ -35,8 +35,8 @@ const MODEL_MAPPING = {
   'glm-4.7' : 'z-ai/glm-4.7',
   'stepfun' : 'stepfun-ai/step-3.5-flash',
   'llama3.1-405b' : 'meta/llama-3.1-405b-instruct',
-  'llama3.3-70b' : 'meta/llama-3.3-70b-instruct'
-
+  'llama3.3-70b' : 'meta/llama-3.3-70b-instruct',
+  'glm5.1' : 'z-ai/glm-5.1'
 };
 
 // Health check endpoint
